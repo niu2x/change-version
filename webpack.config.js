0ab40fb5eebd67dda53e82440d09082af1d1391f
niu2x <niu2x@icloud.com>
2025-03-16 01:25:33 +0800

    fix

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+// import path from 'path'
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// export default {
+//   entry: './src/main.js',
+//   output: {
+//     filename: 'main.js',
+//     path: path.resolve(__dirname, 'dist'),
+//   }
+// }
